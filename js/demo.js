fcb57e702a5ccab45c9ad2bbc7aa97acae9bd2c7
Justin Windle <j@qwiki.com>
2012-07-09 19:17:44 -0400

    Updated example. Added globals for easy access to math functions. Lazy loading of Mr. Doob's Stats if specified.

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,14 @@
 
+/**
+ * --------------------------------------------------
+ *
+ *  A simple demo base class.
+ *
+ *  TODO: Handle touch events transparently.
+ *
+ * --------------------------------------------------
+ */
+
 var Demo;
 
 Demo = (function() {
@@ -6,7 +16,7 @@ Demo = (function() {
     /**
      * --------------------------------------------------
      *
-     * Polyfills
+     *  Polyfills
      *
      * --------------------------------------------------
      */
@@ -115,78 +125,79 @@ Demo = (function() {
     /**
      * --------------------------------------------------
      *
-     * Demo
+     *  Demo
      *
      * --------------------------------------------------
      */
 
     function Demo( options ) {
 
+        // Provide some useful global properties & methods.
+        this._extend( window, this._globals );
+
         // Merge options with defaults.
-        this.options = this._extend( options, this._defaults );
+        options = this._extend( options || {}, this._defaults );
+        this._extend( this, options );
 
-        // Bind all methods to this scope.
-        this._bindAll();
-        this._bindAll( this.options, this );
-
-        // Initialise properties.
-        this.currentTime = +new Date();
-        this.previousTime = this.currentTime;
-
-        this.mouseX = 0.0;
-        this.mouseY = 0.0;
-        this.oMouseX = 0.0;
-        this.oMouseY = 0.0;
-
-        // Create container.
-        this.domElement = document.createElement( 'div' );
-        this.domElement.style.width = '100%';
-        this.domElement.style.height = '100%';
-        this.options.container.appendChild( this.domElement );
-
-        // Create rendering context.
-        switch( this.options.type ) {
-
-            case Demo.CANVAS:
-
-                if ( !!window.CanvasRenderingContext2D ) {
-
-                    this.canvas = document.createElement( 'canvas' );
-                    this.ctx = this.canvas.getContext( '2d' );
-                    this.domElement.appendChild( this.canvas );
-
-                } else {
-
-                    throw new Error( 'CanvasRenderingContext2D not supported' );
-                }
-
-                break;
+        if ( this.autostart ) {
+            this.start();
         }
-
-        // Bind event handlers.
-        addEvent( this.domElement, 'click', this._onClick );
-        addEvent( this.domElement, 'mousemove', this._onMouseMove );
-        addEvent( window, 'resize', this._onResize );
-
-        // Initialise.
-        this._onResize();
-        this.options.setup();
-        this._update( this.currentTime );
     }
 
     Demo.CANVAS = 'canvas';
+
+    Demo.prototype._globals = {
+
+        PI: Math.PI,
+        TWO_PI: Math.PI * 2,
+        HALF_PI: Math.PI / 2,
+        QUATER_PI: Math.PI / 4,
+
+        sin: Math.sin,
+        cos: Math.cos,
+        tan: Math.tan,
+        pow: Math.pow,
+        exp: Math.exp,
+        min: Math.min,
+        max: Math.max,
+        sqrt: Math.sqrt,
+        atan: Math.atan,
+        atan2: Math.atan2,
+        ceil: Math.ceil,
+        round: Math.round,
+        floor: Math.floor,
+
+        random: function( min, max ) {
+
+            // Allow for random string / array access.
+            if ( min && typeof min.length === 'number' && !!min.length ) {
+                return min[ Math.floor( Math.random() * min.length ) ];
+            }
+
+            if ( typeof max !== 'number' ) {
+                max = min || 1.0;
+                min = 0;
+            }
+
+            return min + Math.random() * (max - min);
+        }
+    };
 
     Demo.prototype._defaults = {
 
         type        : Demo.CANVAS,
         fullscreen  : true,
+        autoclear   : true,
+        autostart   : true,
         container   : document.body,
 
         setup       : function() {},
         draw        : function( ctx, dt ) {},
         resize      : function( width, height ) {},
         mousemove   : function( event ) {},
-        mousedown   : function( event ) {}
+        click       : function( event ) {},
+        keydown     : function( event ) {},
+        keyup       : function( event ) {}
     };
 
     Demo.prototype._extend = function( child, parent ) {
@@ -212,30 +223,126 @@ Demo = (function() {
         }
     },
 
+    Demo.prototype.start = function() {
+
+        // Bind all methods to this scope.
+        this._bindAll();
+
+        // Initialise properties.
+        this.currentTime = +new Date();
+        this.previousTime = this.currentTime;
+
+        this.mouseX = 0.0;
+        this.mouseY = 0.0;
+        this.oMouseX = 0.0;
+        this.oMouseY = 0.0;
+
+        // Create container.
+        this.domElement = document.createElement( 'div' );
+        this.container.appendChild( this.domElement );
+
+        // Create rendering context.
+        switch( this.type ) {
+
+            case Demo.CANVAS:
+
+                if ( !!window.CanvasRenderingContext2D ) {
+
+                    this.canvas = document.createElement( 'canvas' );
+                    this.ctx = this.canvas.getContext( '2d' );
+                    this.domElement.appendChild( this.canvas );
+
+                } else {
+
+                    throw new Error( 'CanvasRenderingContext2D not supported' );
+                }
+
+                break;
+        }
+
+        // Bind event handlers.
+        addEvent( this.domElement, 'click', this._onClick );
+        addEvent( this.domElement, 'mousemove', this._onMouseMove );
+        addEvent( window, 'keydown', this._onKeyDown );
+        addEvent( window, 'keyup', this._onKeyUp );
+        addEvent( window, 'resize', this._onResize );
+
+        // Add stats.
+        if ( this.stats ) {
+
+            if ( typeof Stats === 'function' ) {
+
+                this._initStats();
+
+            } else {
+
+                var script = document.createElement( 'script' );
+                script.setAttribute( 'type', 'text/javascript' );
+                script.setAttribute( 'src', 'https://raw.github.com/mrdoob/stats.js/d5f5aa40a24a6d5667ecbcef20c13c75cf236bcd/build/Stats.js' );
+                script.onload = this.onreadystatechange = this._initStats;
+
+                document.body.appendChild( script );
+            }
+        }
+
+        // Initialise.
+        this._onResize();
+        this.setup();
+        this._update( this.currentTime );
+    },
+
     Demo.prototype._update = function( time ) {
+
+        if ( this._stats ) {
+            this._stats.begin();
+        }
 
         this.previousTime = this.currentTime;
         this.currentTime = time;
 
-        this.options.draw( this.ctx, this.currentTime - this.previousTime );
+        if ( this.autoclear ) {
+            this.clear();
+        }
+
+        this.draw( this.ctx, this.currentTime - this.previousTime );
 
         requestAnimationFrame( this._update );
+
+        if ( this._stats ) {
+            this._stats.end();
+        }
+    };
+
+    Demo.prototype._initStats = function() {
+
+        if ( typeof Stats === 'function' ) {
+
+            this._stats = new Stats();
+            this._stats.setMode(0);
+
+            // Align top-left
+            this._stats.domElement.style.position = 'absolute';
+            this._stats.domElement.style.right = '10px';
+            this._stats.domElement.style.top = '10px';
+
+            document.body.appendChild( this._stats.domElement );
+        }
     };
 
     Demo.prototype._onResize = function( event ) {
 
-        if ( this.options.fullscreen ) {
+        if ( this.fullscreen ) {
 
             this.width = window.innerWidth;
             this.height = window.innerHeight;
 
         } else {
 
-            this.width = this.options.width;
-            this.height = this.options.height;
+            this.width = this.width;
+            this.height = this.height;
         }
 
-        switch ( this.options.type ) {
+        switch ( this.type ) {
 
             case Demo.CANVAS:
 
@@ -245,12 +352,15 @@ Demo = (function() {
                 break;
         }
 
-        this.options.resize( this.width, this.height );
+        this.domElement.style.width = this.width;
+        this.domElement.style.height = this.height;
+
+        this.resize( this.width, this.height );
     },
 
     Demo.prototype._onClick = function( event ) {
 
-        this.options.mousedown( event );
+        this.click( event );
     };
 
     Demo.prototype._onMouseMove = function( event ) {
@@ -261,7 +371,31 @@ Demo = (function() {
         this.mouseX = event.clientX;
         this.mouseY = event.clientY;
 
-        this.options.mousemove( event );
+        this.mousemove( event );
+    };
+
+    Demo.prototype._onKeyDown = function( event ) {
+
+        this.key = event.keyCode;
+        this.keydown( event );
+    },
+
+    Demo.prototype._onKeyUp = function( event ) {
+
+        this.key = null;
+        this.keyup( event );
+    },
+
+    Demo.prototype.clear = function() {
+
+        switch ( this.type ) {
+
+            case Demo.CANVAS:
+
+                this.canvas.width = this.canvas.width;
+
+                break;
+        }
     };
 
     return Demo;
